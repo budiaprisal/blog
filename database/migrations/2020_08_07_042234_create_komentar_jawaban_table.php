a10d9afba75abde949dee86d7cb59cccb445abba
=budi_aprisal <sam@google.com>
2020-08-07 11:27:10 +0700

    tugas migration

---
+++ database/migrations/2020_08_07_042234_create_komentar_jawaban_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKomentarJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('komentar_jawaban', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('isi');
+            $table->date('tanggal_dibuat');
+
+            $table->unsignedBigInteger('profile_id')->nullable();
+
+            $$table->foreign('profile_id')->references('id')->on('profil');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('komentar_jawaban');
+    }
+}
